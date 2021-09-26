2cfdd344fc694871a66fb1605931672436257b9e
CHIRATCHAYA\Asus <blak_8_white@hotmail.com>
2021-09-26 14:03:46 +0700

    update project

---
+++ Bomber_Server/Assets/Scripts/Models/InitDataModel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class InitDataModel : BaseModel
+{
+    public static readonly string CLASS_NAME = typeof(InitDataModel).Name;
+
+    public InitDataModel() : base(CLASS_NAME)
+    {
+
+    }
+
+    public int PlayerId { get; set; }
+    public List<CreatePlayerModel> CreatePlayerModels { get; set; } = new List<CreatePlayerModel>();
+}
