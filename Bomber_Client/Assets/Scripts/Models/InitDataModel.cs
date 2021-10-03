25ddd41e7ac52ec0a4b04c32facf128b81a60b58
CHIRATCHAYA\Asus <blak_8_white@hotmail.com>
2021-10-03 11:37:34 +0700

    add bomb

--- Bomber_Client/Assets/Scripts/Models/InitDataModel.cs
+++ Bomber_Client/Assets/Scripts/Models/InitDataModel.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 using System.Collections.Generic;
 
 public class InitDataModel : BaseModel
@@ -12,4 +11,5 @@ public class InitDataModel : BaseModel
     public int PlayerId { get; set; }
     public List<CreatePlayerModel> CreatePlayerModels { get; set; } = new List<CreatePlayerModel>();
     public Dictionary<int, Vector3Model> CreateCoins { get; set; } = new Dictionary<int, Vector3Model>();
+    public List<BombModel> Bombs { get; set; } = new List<BombModel>();
 }
